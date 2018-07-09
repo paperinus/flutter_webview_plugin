d45d0e429e42e1f901a857e89d7e24bd08731ffd
daniele <daniele.lama@netfarm.com>
2018-07-09 12:18:41 +0200

    Native Webview in a Widget

--- lib/src/webview_generic.dart
+++ lib/src/webview_generic.dart
@@ -14,6 +14,7 @@ class WebviewGeneric extends StatefulWidget {
   final bool withZoom;
   final bool withLocalStorage;
   final bool withLocalUrl;
+  final GlobalKey<_WebviewGenericState> key;
 
   WebviewGeneric({
     Key key,
@@ -28,6 +29,18 @@ class WebviewGeneric extends StatefulWidget {
     this.withLocalUrl,
   }) : super(key: key);
 
+  static Rect getRectFromKey(GlobalKey<_WebviewGenericState> globalKey) {
+    var object = globalKey?.currentContext?.findRenderObject();
+    var translation = object?.getTransformTo(null)?.getTranslation();
+    var size = object?.semanticBounds?.size;
+
+    if (translation != null && size != null) {
+      return new Rect.fromLTWH(translation.x, translation.y, size.width, size.height);
+    } else {
+      return null;
+    }
+  }
+
   @override
   _WebviewGenericState createState() => new _WebviewGenericState();
 }
@@ -36,7 +49,7 @@ class _WebviewGenericState extends State<WebviewGeneric> {
   final webviewReference = new FlutterWebviewPlugin();
   Rect _rect;
   Timer _resizeTimer;
-  var globalKey = RectGetter.createGlobalKey();
+  var globalKey = new GlobalKey<_WebviewGenericState>();
 
   void initState() {
     super.initState();
@@ -52,16 +65,13 @@ class _WebviewGenericState extends State<WebviewGeneric> {
   @override
   Widget build(BuildContext context) {
 
-
-    return new RectGetter(
-      key: globalKey,
-      child: new Container(
+      return new Container(
+        key: globalKey,
         child: new LayoutBuilder(builder:
             (BuildContext context, BoxConstraints constraints) {
               if (_rect == null) {
                 _rect = _buildRect(context);
                 _resizeTimer?.cancel();
-
                 webviewReference.launch(widget.url,
                     withJavascript: widget.withJavascript,
                     clearCache: widget.clearCache,
@@ -72,12 +82,12 @@ class _WebviewGenericState extends State<WebviewGeneric> {
                     withZoom: widget.withZoom,
                     withLocalStorage: widget.withLocalStorage,
                     withLocalUrl: widget.withLocalUrl);
-                _resizeTimer = new Timer(new Duration(milliseconds: 300), () {
+                _resizeTimer = new Timer(new Duration(milliseconds: 100), () {
                   _rect = _buildRect(context);
                   webviewReference.resize(_rect);
                 });
               } else {
-                Rect rect = new Rect.fromLTWH(0.0, 0.0, 0.0, 0.0);//_buildRect(context);
+                Rect rect = _buildRect(context);
                 if (_rect != rect) {
                   _rect = rect;
                   webviewReference.resize(_rect);
@@ -89,35 +99,14 @@ class _WebviewGenericState extends State<WebviewGeneric> {
                   });
                 }
               }
-
               return new Container();
         }),
-      ),
     );
-
-
-
-
   }
 
   Rect _buildRect(BuildContext context) {
-
-/*
-    if (widget.bottomNavigationBar != null) {
-      height -=
-          56.0 + mediaQuery.padding.bottom; // todo(lejard_h) find a way to determine bottomNavigationBar programmatically
-    }
-
-    if (widget.persistentFooterButtons != null) {
-      height -=
-          53.0; // todo(lejard_h) find a way to determine persistentFooterButtons programmatically
-      if (widget.bottomNavigationBar == null){
-         height -= mediaQuery.padding.bottom;
-      }
-    }
-*/
     try{
-      return RectGetter.getRectFromKey(globalKey);
+      return WebviewGeneric.getRectFromKey(globalKey);
 
     }catch(err){
       return new Rect.fromLTWH(0.0, 0.0, 0.0, 0.0);
@@ -125,41 +114,4 @@ class _WebviewGenericState extends State<WebviewGeneric> {
     //
   }
 
-}
-
-class RectGetter extends StatefulWidget {
-  final GlobalKey<_RectGetterState> key;
-  final Widget child;
-
-  /// Use this static method to get child`s rectangle information when had a custom globalkey
-  static Rect getRectFromKey(GlobalKey<_RectGetterState> globalKey) {
-    var object = globalKey?.currentContext?.findRenderObject();
-    var translation = object?.getTransformTo(null)?.getTranslation();
-    var size = object?.semanticBounds?.size;
-
-    if (translation != null && size != null) {
-      return new Rect.fromLTWH(translation.x, translation.y, size.width, size.height);
-    } else {
-      return null;
-    }
-  }
-
-  /// create a custom globalkey , use this way to avoid type exception in dart2 .
-  static GlobalKey<_RectGetterState> createGlobalKey() {
-    return new GlobalKey<_RectGetterState>();
-  }
-  /// constructor with key passed , and then you can get child`s rect by using RectGetter.getRectFromKey(key)
-  RectGetter({@required this.key, @required this.child}) : super(key: key);
-
-  Rect getRect() {
-    return getRectFromKey(this.key);
-  }
-
-  @override
-  _RectGetterState createState() => new _RectGetterState();
-}
-
-class _RectGetterState extends State<RectGetter> {
-  @override
-  Widget build(BuildContext context) => widget.child;
 }
